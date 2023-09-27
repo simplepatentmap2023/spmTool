6a2a796ae2d89a7089c29579c08b3e6eefb5067f
Toshitsugu NAKAJIMA <simplepatentmap@gmail.com>
2023-09-28 08:18:32 +0900

    グラフの表示 TOP10のグラフの表示には成功。 （順位の表示など課題が残る。）

--- TGraph.py
+++ TGraph.py
@@ -11,28 +11,10 @@ class DrawGraph():
     # import seaborn as sns
     # sns.set(font="IPAexGothic")  # 日本語フォント設定
 
-    def drawBarH(self, df, barColor):
-        fig, axes = plt.subplots(3, 1, sharex=True, figsize=(9, 6))
-
-        top10 = df[0:9].sort_values(ascending=True)
-        top20 = df[10:19].sort_values(ascending=True)
-        top30 = df[20:29].sort_values(ascending=True)
-
-        axes[0].barh(top10.index[0: 9:], top10.values[0: 9], color=barColor)
-        axes[0].set_title('筆頭出願人TOP 1-10')
-        axes[1].barh(top20.index[0: 9], top20.values[0: 9], color=barColor)
-        axes[1].set_title('筆頭出願人TOP11-20')
-        axes[2].barh(top30.index[0: 9], top30.values[0: 9], color=barColor)
-        axes[2].set_title('筆頭出願人TOP21-30')
-
-
+    def drawBarH(self, df, rank,  barColor, title):
+        fig, axes = plt.subplots(rank, 1, sharex=True, figsize=(6, 8))
+        for i in range(rank):
+            axes[i].set_title(f'{title} TOP{i*10+1}-{i*10+10}')
+            axes[i].barh(df[i:i+10].index, df[i:i+10].values, label=df[i:i+10].values, color=barColor)
+            axes[i].invert_yaxis()
         return fig
-
-    # df = pd.read_csv('/content/drive/MyDrive/明細書：スタッド溶接、公知日20010101-20221231.csv')
-    # series = df['出願人/権利者'].value_counts()
-    # img = drawBarH(series=series, barColor='#ffa07a', title='筆頭出願人')
-    # img
-    #
-    # series = df['FI'].value_counts()
-    # img2 = drawBarH(series=series, barColor='gray', title='FI')
-    # img
